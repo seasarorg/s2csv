8949390058ebd22053a795dba0535f715927abf4
newta <t.newtaro@gmail.com>
2008-11-07 15:29:18 +0000

    parseAllの例を追加
    git-svn-id: https://www.seasar.org/svn/sandbox/s2csv/trunk@33 b4bf6cb1-7fc1-43ed-b5dc-777925a6a1a9

--- s2csv-tutorial/src/main/java/org/seasar/s2csv/tutorial/service/DeptService.java
+++ s2csv-tutorial/src/main/java/org/seasar/s2csv/tutorial/service/DeptService.java
@@ -11,6 +11,7 @@ import java.util.List;
 import org.seasar.framework.beans.util.Beans;
 import org.seasar.s2csv.csv.S2CSVParseCtrl;
 import org.seasar.s2csv.csv.S2CSVWriteCtrl;
+import org.seasar.s2csv.csv.exception.runtime.CSVValidationResultException;
 import org.seasar.s2csv.csv.exception.runtime.CSVValidationResultRuntimeException;
 import org.seasar.s2csv.csv.factory.S2CSVCtrlFactory;
 import org.seasar.s2csv.csv.util.S2CSVUtil;
@@ -26,136 +27,178 @@ public class DeptService extends AbstractService<Dept> {
 	 * CSVコントローラを作るファクトリです
 	 */
 	public S2CSVCtrlFactory csvCtrlFactory;
-	
-    public Dept findById(Long id) {
-        return select().id(id).getSingleResult();
-    }
-    
-    /**
-     * DeptテーブルのデータをCSV形式で出力する
-     * @param fileName
-     * @throws IOException
-     */
-    public void outCsv1(String fileName) throws IOException {
-    	Writer writer = new FileWriter(fileName);
-    	
-    	S2CSVWriteCtrl<DeptCsv> csv_writer = 
-    		csvCtrlFactory.getWriteController(DeptCsv.class, writer);
-    	
-    	List<Dept> all = super.findAll();
-    	
-    	for(Dept o : all){
-    		
-    		DeptCsv deptCsv = Beans.createAndCopy(DeptCsv.class, o).execute();
 
-        	csv_writer.write(deptCsv);
-    	}
-    	
-    	csv_writer.close();
-    }
-    
-    /**
-     * DeptテーブルのデータをCSV形式で出力する
-     * @param fileName
-     * @throws IOException
-     */
-    public void outCsv2(String fileName) throws IOException {
+	public Dept findById(Long id) {
+		return select().id(id).getSingleResult();
+	}
 
-    	Writer writer = new FileWriter(fileName);
-    	S2CSVUtil.s2jdbcToCsv(DeptCsv.class, select(), writer);
-    }
-    
-    /**
-     * Deptテーブルのデータを削除し、
-     * CSVファイルデータをDeptテーブルに登録する。
-     * @param fileName
-     * @throws FileNotFoundException
-     */
-    public void inCsv1(String fileName) throws FileNotFoundException {
-    	
-    	Reader reader = new FileReader(fileName);
-    	jdbcManager.callBySql("delete from dept").execute();
-    	
-    	 S2CSVParseCtrl<DeptCsv> csv_parser = 
-    		 csvCtrlFactory.getParseController(DeptCsv.class, reader);
-    	 
-    	 //parse時のバリデーションチェックをオフにして
-    	 //自分でvalidateメソッドを呼ぶ
-    	 csv_parser.setValidateFlag(false);
-    	 
-    	 while(csv_parser.readNext()){
-    		 CSVValidateResult validateResult = csv_parser.validate();
-    		 
-    		 if(validateResult != null){
-    			 CSVExampleUtil.showErrMessage(validateResult);
-    			continue;
-    		 }
-    		 
-    		 DeptCsv deptCsv = csv_parser.parse();
-    		 
-    		 Dept dept = Beans.createAndCopy(Dept.class,deptCsv).execute();
-    		 
-    		 insert(dept);
-    	 }
-    }
-    /**
-     * Deptテーブルのデータを削除し、
-     * CSVファイルデータをDeptテーブルに登録する。
-     * @param fileName
-     * @throws FileNotFoundException
-     */
-    public void inCsv2(String fileName) throws FileNotFoundException {
-    	
-    	Reader reader = new FileReader(fileName);
-    	jdbcManager.callBySql("delete from dept").execute();
-    	
-    	 S2CSVParseCtrl<DeptCsv> csv_parser = 
-    		 csvCtrlFactory.getParseController(DeptCsv.class, reader);
-    	 
-    	 while(csv_parser.readNext()){
-    		 
-    		 try{
-	    		 DeptCsv deptCsv = csv_parser.parse();
-	    		 Dept dept = Beans.createAndCopy(Dept.class,deptCsv).execute();
-	    		 insert(dept);
-	    		 
-    		 }catch(CSVValidationResultRuntimeException e){
-    			 CSVExampleUtil.showErrMessage(e.getValidateResult());
-    		 }
-    	 }
-    }
-    /**
-     * Deptテーブルのデータを削除し、
-     * CSVファイルデータをDeptテーブルに登録する。
-     * @param fileName
-     * @throws FileNotFoundException
-     */
-    public void inCsv3(String fileName) throws FileNotFoundException {
-    	
-    	Reader reader = new FileReader(fileName);
-    	jdbcManager.callBySql("delete from dept").execute();
-    	
-    	
-    	List<CSVValidateResult> result =
-    		S2CSVUtil.csvValidateToS2Jdbc(DeptCsv.class,Dept.class,reader);
-    	
-    	
-    	CSVExampleUtil.showErrMessage(result);
-    }
+	/**
+	 * DeptテーブルのデータをCSV形式で出力する
+	 * @param fileName
+	 * @throws IOException
+	 */
+	public void outCsv1(String fileName) throws IOException {
+		Writer writer = new FileWriter(fileName);
 
-    /**
-     * Deptテーブルのデータを削除し、
-     * CSVファイルデータをDeptテーブルに登録する。
-     * @param fileName
-     * @throws FileNotFoundException
-     */
-    public void inCsv4(String fileName) throws FileNotFoundException {
-    	
-    	Reader reader = new FileReader(fileName);
-    	jdbcManager.callBySql("delete from dept").execute();
-    	
-    	S2CSVUtil.csvToS2Jdbc(DeptCsv.class,Dept.class,reader);
-    }
-    
-    
+		S2CSVWriteCtrl<DeptCsv> csv_writer = csvCtrlFactory.getWriteController(
+				DeptCsv.class, writer);
+
+		List<Dept> all = super.findAll();
+
+		for (Dept o : all) {
+
+			DeptCsv deptCsv = Beans.createAndCopy(DeptCsv.class, o).execute();
+
+			csv_writer.write(deptCsv);
+		}
+
+		csv_writer.close();
+	}
+
+	/**
+	 * DeptテーブルのデータをCSV形式で出力する
+	 * @param fileName
+	 * @throws IOException
+	 */
+	public void outCsv2(String fileName) throws IOException {
+
+		Writer writer = new FileWriter(fileName);
+		S2CSVUtil.s2jdbcToCsv(DeptCsv.class, select(), writer);
+	}
+
+	/**
+	 * Deptテーブルのデータを削除し、
+	 * CSVファイルデータをDeptテーブルに登録する。
+	 * @param fileName
+	 * @throws FileNotFoundException
+	 */
+	public void inCsv1(String fileName) throws FileNotFoundException {
+
+		Reader reader = new FileReader(fileName);
+		jdbcManager.callBySql("delete from dept").execute();
+
+		S2CSVParseCtrl<DeptCsv> csv_parser = csvCtrlFactory.getParseController(
+				DeptCsv.class, reader);
+
+		//parse時のバリデーションチェックをオフにして
+		//自分でvalidateメソッドを呼ぶ
+		csv_parser.setValidateFlag(false);
+
+		while (csv_parser.readNext()) {
+			CSVValidateResult validateResult = csv_parser.validate();
+
+			if (validateResult != null) {
+				CSVExampleUtil.showErrMessage(validateResult);
+				continue;
+			}
+
+			DeptCsv deptCsv = csv_parser.parse();
+
+			Dept dept = Beans.createAndCopy(Dept.class, deptCsv).execute();
+
+			insert(dept);
+		}
+	}
+
+	/**
+	 * Deptテーブルのデータを削除し、
+	 * CSVファイルデータをDeptテーブルに登録する。
+	 * @param fileName
+	 * @throws FileNotFoundException
+	 */
+	public void inCsv2(String fileName) throws FileNotFoundException {
+
+		Reader reader = new FileReader(fileName);
+		jdbcManager.callBySql("delete from dept").execute();
+
+		S2CSVParseCtrl<DeptCsv> csv_parser = csvCtrlFactory.getParseController(
+				DeptCsv.class, reader);
+
+		while (csv_parser.readNext()) {
+
+			try {
+				DeptCsv deptCsv = csv_parser.parse();
+				Dept dept = Beans.createAndCopy(Dept.class, deptCsv).execute();
+				insert(dept);
+
+			} catch (CSVValidationResultRuntimeException e) {
+				CSVExampleUtil.showErrMessage(e.getValidateResult());
+			}
+		}
+	}
+
+	/**
+	 * Deptテーブルのデータを削除し、
+	 * CSVファイルデータをDeptテーブルに登録する。
+	 * @param fileName
+	 * @throws FileNotFoundException
+	 */
+	public void inCsv3(String fileName) throws FileNotFoundException {
+
+		Reader reader = new FileReader(fileName);
+		jdbcManager.callBySql("delete from dept").execute();
+
+		List<CSVValidateResult> result = S2CSVUtil.csvValidateToS2Jdbc(
+				DeptCsv.class, Dept.class, reader);
+
+		CSVExampleUtil.showErrMessage(result);
+	}
+
+	/**
+	 * Deptテーブルのデータを削除し、
+	 * CSVファイルデータをDeptテーブルに登録する。
+	 * @param fileName
+	 * @throws FileNotFoundException
+	 */
+	public void inCsv4(String fileName) throws FileNotFoundException {
+
+		Reader reader = new FileReader(fileName);
+		jdbcManager.callBySql("delete from dept").execute();
+
+		S2CSVUtil.csvToS2Jdbc(DeptCsv.class, Dept.class, reader);
+	}
+
+	/**
+	 * Deptテーブルのデータを削除し、
+	 * CSVファイルデータをDeptテーブルに登録する。
+	 * @param fileName
+	 * @throws FileNotFoundException
+	 */
+	public void inCsv5(String fileName) throws FileNotFoundException {
+
+		Reader reader = new FileReader(fileName);
+		jdbcManager.callBySql("delete from dept").execute();
+
+		S2CSVParseCtrl<DeptCsv> csv_parser = csvCtrlFactory.getParseController(
+				DeptCsv.class, reader);
+
+		List<DeptCsv> list = null;
+		List<CSVValidateResult> validateErrList = null;
+		
+		try {
+			//全ての行を一括でパースする。
+			//バリデーションエラーがある場合はあとでまとめてExceptionに含まれて返る
+			list = csv_parser.parseAll();
+		} catch (CSVValidationResultException e) {
+			
+			//バリデーションエラーが無かった行はこれで取得できる。
+			list = e.getDataList();
+			
+			//バリデーションエラーのあった行はこれでバリデーションエラーメッセージ情報が取得できる
+			validateErrList = e.getValidateResultList();
+		}
+		
+		if(list != null){
+			//エラーの無かったデータはInsert
+			for(DeptCsv deptCsv : list){
+				Dept dept = Beans.createAndCopy(Dept.class, deptCsv).execute();
+				insert(dept);
+			}
+		}
+
+		if (validateErrList != null) {
+			CSVExampleUtil.showErrMessage(validateErrList);
+		}
+	}
+
 }
